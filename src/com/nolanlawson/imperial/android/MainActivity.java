030e57c4d5d44e5809ffaffabd73ee20c6f3fb18
Nolan Lawson <nolan.lawson@gmail.com>
2013-04-14 13:19:58 +0200

    initial commit

---
+++ src/com/nolanlawson/imperial/android/MainActivity.java
@@ -0,0 +1,30 @@
+package com.nolanlawson.imperial.android;
+
+import org.apache.cordova.DroidGap;
+
+import android.os.Bundle;
+
+public class MainActivity extends DroidGap {
+
+    private static final int SPLASH_DISPLAY_TIME = 1000;
+   
+    
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        
+        // Config properties in addition to res/xml/config.xml.  See:
+        // https://github.com/apache/incubator-cordova-android/blob/master/framework/src/org/apache/cordova/DroidGap.java
+        super.setIntegerProperty("backgroundColor", R.color.splash_bg);
+        super.setIntegerProperty("loadUrlTimeoutValue", 120000);
+        
+        setContentView(R.layout.activity_main);
+        
+        startCordova();
+    }
+
+    private void startCordova() {
+        super.loadUrl("file:///android_asset/www/index.html", SPLASH_DISPLAY_TIME);
+    }
+    
+}
