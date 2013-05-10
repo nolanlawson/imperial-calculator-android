e00de3df5e019a171252de226a9d06d6e8845806
Nolan Lawson <nolan.lawson@gmail.com>
2013-05-10 18:17:49 +0200

    prepare for 1.0.4 release

--- build_cleanly.sh
+++ build_cleanly.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+# builds the Android application using the nearby imperial-calculator WWW files.
+# usage: build_cleanly.sh [uninstall_first] [skip_install_entirely]
+#
+
+uninstall_first=$1
+skip_install_entirely=$2
 
 sed_command='sed'
 
@@ -8,8 +14,13 @@ cp -r ../imperial-calculator/* assets/www
 $sed_command -i '/<\/body>/i<script src="phonegap.js"><\/script><script src="share.js"><\/script>' assets/www/index.html
 rm -fr assets/www/gimp/
 
+if [[ $skip_install_entirely ]]; then
+	exit 0;
+fi;
 
-#adb uninstall com.nolanlawson.imperial.android
+if [[ $uninstall_first ]]; then
+	adb uninstall com.nolanlawson.imperial.android
+fi
 ant clean debug install
 adb shell am start -a android.intent.action.MAIN -n com.nolanlawson.imperial.android/.MainActivity
 
